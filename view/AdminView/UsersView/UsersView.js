55d978edec147aeac0e7bdf1b3645dbe122050e8
jankominek <janekkominek2000@gmail.com>
2022-11-24 18:28:31 +0100

    blocking user

--- view/AdminView/UsersView/UsersView.js
+++ view/AdminView/UsersView/UsersView.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { Button } from "../../../components/Button/Button";
 import { withLayout } from "../../../layout/pageLayout/PageLayout";
@@ -14,33 +15,46 @@ import {
 	UsersViewWrapper,
 	UsersViewWrapperScrollView,
 } from "./UsersView.styled";
+import axios from "axios";
 
-const users = [
-	{ email: "email 1", blocked: false },
-	{ email: "email 2", blocked: false },
-	{ email: "email 3", blocked: true },
-	{ email: "email 4", blocked: false },
-];
-
-const userList = users.map((user) => (
-	<UserField
-		background={
-			user.blocked ? colorPallete.blockedRed : colorPallete.nonBlockedGreen
-		}
-	>
-		<UserFieldNameBox>
-			<UserFieldName numberOfLines={1}>{user.email}</UserFieldName>
-		</UserFieldNameBox>
-		<UserFieldButtons>
-			{user.blocked ? (
-				<Button text="Unlock" width="100%" />
-			) : (
-				<Button text="Block" width="100%" />
-			)}
-		</UserFieldButtons>
-	</UserField>
-));
 const UsersViewContainer = () => {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		axios.get("/getAllUsers").then((response) => {
+			setUsers(response.data);
+		});
+	}, []);
+
+	const blockUser = (userID) => {
+		console.log("userid: ", userID);
+		axios.post(`/blockUser/${userID}`).catch((err) => {
+			console.log(err);
+		});
+	};
+	const userList = users.map((user) => (
+		<UserField
+			background={
+				user.blocked ? colorPallete.blockedRed : colorPallete.nonBlockedGreen
+			}
+		>
+			<UserFieldNameBox>
+				<UserFieldName numberOfLines={1}>{user.email}</UserFieldName>
+			</UserFieldNameBox>
+			<UserFieldButtons>
+				{user.blocked ? (
+					<Button text="Unlock" width="100%" />
+				) : (
+					<Button
+						text="Block"
+						width="100%"
+						onPress={() => blockUser(user.userID)}
+					/>
+				)}
+			</UserFieldButtons>
+		</UserField>
+	));
+
 	return (
 		<ViewWrapper>
 			<UsersViewWrapperScrollView>
